b96bde84edc694259be28a1188cf6892730a1080
mon1kasenpai <ballaticseal@gmail.com>
2021-07-18 12:29:20 +1000

    fixed some stuff and got basic sound code down

--- Useless button IOS/ContentView.swift
+++ Useless button IOS/ContentView.swift
@@ -12,14 +12,23 @@ struct ContentView: View {
     var body: some View {
         VStack{
             Text("Press me")
-                .foregroundColor(Color.black)
-            .padding()
-    }
+                .fontWeight(.bold)
+                .font(.system(size: 25))
+                .frame(width: 150.0, height: 75.0)
+                .foregroundColor(Color.red)
+                .padding(.vertical, 5.0)
+        }
+        VStack{
+            HStack{
+                
+            }
+    
 }
 }
-//ignore this future me
+//don't modify this, this is just for the visual previews in xcode. People using VS on windows ignore, this dosent effect you
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
---
+++ Useless button IOS/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  Useless button IOS
+//
+//  Created by Lachlan Forbes on 18/7/21.
+//
+
+// import Foundation
+//import UIKit
+//import AVFoundation
+
+//class Viewcontroller: UIViewController {
+   //
+   // var audioPlayer = AVAudioPlayer()
+    
+   // override func viewDidLoad() {
+    //    super.viewDidLoad()
+        
+   //     do{
+       //     audioPlayer = try AVAudioPlayer(contentsOf: URL.int())
+     //       bundle.main.path(forresource: "button.png", offType: "error.wav)
+   //     } catch {
+ //           print(error)
+       //
+     //   }
+   // }
+ //   @IBAction func Play(_ sender: Any) {
+      //  audioPlayer.play()
+//    }
+// }
